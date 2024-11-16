698f7c592234f30dabe157e04d40986ae4a27662
tuan anh <>
2024-11-16 21:59:13 +0700

    update UI parentDashboard

--- app/src/main/java/com/example/amp_g01_reading_app/ui/settings/dashboard_management/ParentDashboardFragment.java
+++ app/src/main/java/com/example/amp_g01_reading_app/ui/settings/dashboard_management/ParentDashboardFragment.java
@@ -1,4 +1,5 @@
 package com.example.amp_g01_reading_app.ui.settings.dashboard_management;
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
@@ -12,13 +13,24 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.amp_g01_reading_app.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParentDashboardFragment extends Fragment {
 
     private TextView tvUserName, tvUserAge;
-    private ProgressBar pbBooksRead, pbVideosWatched, pbArticlesRead, pbTestsFinished;
-    private LinearLayout barChartStats;
     private ImageView closeButton;
+
+    private BarChart barChart;
+
     public ParentDashboardFragment() {
         // Required empty public constructor
     }
@@ -33,19 +45,40 @@ public class ParentDashboardFragment extends Fragment {
         tvUserName = view.findViewById(R.id.tv_user_name);
         closeButton = view.findViewById(R.id.icon_back);
         tvUserAge = view.findViewById(R.id.tv_user_age);
-        pbBooksRead = view.findViewById(R.id.pb_books_read);
-        pbVideosWatched = view.findViewById(R.id.pb_videos_watched);
-        pbArticlesRead = view.findViewById(R.id.pb_articles_read);
-        pbTestsFinished = view.findViewById(R.id.pb_tests_finished);
-        barChartStats = view.findViewById(R.id.ll_stats);
+
+        barChart = view.findViewById(R.id.barChart);
 
         // Set sample data
-        tvUserName.setText("Justin");
-        tvUserAge.setText("8 y.o.");
-        pbBooksRead.setProgress(47);
-        pbVideosWatched.setProgress(20);
-        pbArticlesRead.setProgress(15);
-        pbTestsFinished.setProgress(25);
+        tvUserName.setText("Tuấn Anh");
+        tvUserAge.setText("21 tuổi");
+
+        List<BarEntry> readingTimesEntries = new ArrayList<>();
+        readingTimesEntries.add(new BarEntry(0, 24.0f));
+        readingTimesEntries.add(new BarEntry(1, 12.5f));
+        readingTimesEntries.add(new BarEntry(2, 19.8f));
+        readingTimesEntries.add(new BarEntry(3, 10.1f));
+        readingTimesEntries.add(new BarEntry(4, 18.4f));
+        readingTimesEntries.add(new BarEntry(5, 2.7f));
+        readingTimesEntries.add(new BarEntry(6, 18.9f));
+
+        BarDataSet barDataSet = new BarDataSet(readingTimesEntries, "Reading Time");
+        barDataSet.setColor(Color.BLUE);
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.setDescription(null);
+        barChart.setDrawGridBackground(false);
+        barChart.setMaxVisibleValueCount(7);
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}));
+
+        YAxis leftAxis = barChart.getAxisLeft();
+        leftAxis.setAxisMinimum(0f);
+
+        barChart.setData(barData);
+        barChart.invalidate();
 
         closeButton.setOnClickListener(v -> {
             NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_activity_main);
